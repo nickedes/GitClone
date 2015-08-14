23350083e93d1d6793f1ab47724433bd75311ee5
nikhil <nkmittal4994@gmail.com>
2015-08-14 16:35:35 +0530

    clones all the repos of the user.

---
+++ clone.py
@@ -0,0 +1,18 @@
+from git_connect import connect
+import os
+
+
+def getRepos():
+    """Returns all the repos under the user."""
+    github = connect()
+    user = github.get_user()
+    # Get all Repos!
+    all_repos = user.get_repos()
+    return all_repos
+
+
+if __name__ == '__main__':
+    all_repos = getRepos()
+    for repo in all_repos:
+        # Clone the repo.
+        os.system('git clone ' + repo.clone_url)
